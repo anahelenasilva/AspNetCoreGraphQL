9fa1db24b50187aa252ec56d31c2802421994f61
Ana Helena <ahelena-megit@vortx.com.br>
2021-05-19 18:53:09 -0300

    Adding graphql

--- CarvedRock/CarvedRock.Api/Startup.cs
+++ CarvedRock/CarvedRock.Api/Startup.cs
@@ -12,6 +12,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using CarvedRock.Api.Data;
+using CarvedRock.Api.Repositories;
+using GraphQL;
+using CarvedRock.Api.GraphQL;
+using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
 
 namespace CarvedRock.Api
 {
@@ -32,12 +37,26 @@ namespace CarvedRock.Api
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<CarvedRockDbContext>(options => options.UseSqlServer(_config["ConnectionStrings:CarvedRock"]));
+
+            services.AddSingleton<ProductRepository>();
+
+            services.AddScoped<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
+            services.AddScoped<CarvedRockSchema>();
+
+            services.AddGraphQL(o =>
+            {
+                o.ExposeExceptions = true;
+                //o.ComplexityConfiguration =
+                //o.ExposeExceptions //on by default
+            })
+            .AddGraphTypes(ServiceLifetime.Scoped);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, CarvedRockDbContext dbContext)
         {
-            app.UseMvc();
+            app.UseGraphQL<CarvedRockSchema>();
+            app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
             dbContext.Seed();
         }
     }
