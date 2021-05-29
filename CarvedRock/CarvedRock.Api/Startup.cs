ec60c52c06e5278a68527d45c75448015c7e6cae
Ana Helena <anahelenarp@hotmail.com>
2021-05-29 18:41:08 -0300

    Cors; product and reviews

--- CarvedRock/CarvedRock.Api/Startup.cs
+++ CarvedRock/CarvedRock.Api/Startup.cs
@@ -1,22 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CarvedRock.Api.GraphQL;
+using CarvedRock.Api.Repositories;
+using GraphQL;
+using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using CarvedRock.Api.Data;
-using CarvedRock.Api.Repositories;
-using GraphQL;
-using CarvedRock.Api.GraphQL;
-using GraphQL.Server;
-using GraphQL.Server.Ui.Playground;
 
 namespace CarvedRock.Api
 {
@@ -31,7 +22,6 @@ namespace CarvedRock.Api
             _env = env;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -58,9 +48,11 @@ namespace CarvedRock.Api
             services.AddCors();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            //not to the be used in production
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             app.UseGraphQL<CarvedRockSchema>();
             app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
             //dbContext.Seed();
